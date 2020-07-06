9746c9e31b79bc4e2bbe4e07d551d61bb221eaa2
JinWoo_Lee <53039583+dlwlsdn201@users.noreply.github.com>
2020-07-06 12:42:54 +0900

    -added function
    -네이버 뉴스의 각 기사들 태그 요소를 추출하여 제목,사진, 신문사 정보를 추출하는 함수 정의.
    
    *기사 태그 검색이 불가능하여 해결 필요함.

--- naver.py
+++ naver.py
@@ -4,14 +4,12 @@ import requests
 from bs4 import BeautifulSoup
 
 LIMIT = 1
-URL = f"https://news.naver.com/main/list.nhn?mode=LSD&mid=sec&sid1=001"
-
+URL = f"https://news.naver.com/main/list.nhn?mode=LSD&sid1=001&mid=sec&listType=paper&date=20200525&page=1"
+FirstPage = 1
 
 #한 페이지당 마지막 페이지를 불러옴
 def get_last_page():
     result = requests.get(URL, headers={"User-Agent": "Mozilla/5.0"})
-
-    #BeautifulSoup(html_doc, "html.parser")
     soup = BeautifulSoup(result.text, "html.parser")
     pagination = soup.find("div",{"class":"paging"})
     pagination = list(pagination)
@@ -28,4 +26,14 @@ def get_last_page():
     max_page = pages[-1]
     return max_page
 
-get_last_page()
+def extract_all_article(FirstPage, last_page):
+    articles = []
+    for page in range(FirstPage,last_page+1):
+        result = requests.get(
+            f"https://news.naver.com/main/list.nhn?mode=LSD&sid1=001&mid=sec&listType=paper&date=20200525&page={page}",
+            headers={"User-Agent": "Mozilla/5.0"})
+        soup = BeautifulSoup(result.text, "html.parser")
+        results = soup.find_all("div")
+        print(results)
+
+extract_all_article(1,1)
